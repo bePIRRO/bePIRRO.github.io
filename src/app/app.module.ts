9b2a4179565e5ef21bdb63de70fa538aa16f5dd8
bePIRRO <manuel.pirro@gmail.com>
2023-02-13 01:00:54 +0100

    feats: add bootstrap + components + iframe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { HeaderComponent } from './core/header/header.component'
+import { HeaderComponent } from './core/header/header.component';
+import { ListComponent } from './pages/list/list.component';
+import { DetailComponent } from './pages/detail/detail.component'
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, ListComponent, DetailComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
