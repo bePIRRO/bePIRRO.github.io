1438f9c1234fa156ed8e234ae1b2b3ef59aaeb8c
bePIRRO <manuel.pirro@gmail.com>
2023-02-14 00:29:59 +0100

    feat: card + list component

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import sites from '../../../assets/data/sites.json'
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: [],
 })
 export class ListComponent {
+  constructor() {
+    this.getSitesList()
+  }
 
+  sitesList: any[] = []
+
+  getSitesList(): void {
+    sites.sites.forEach((s) => {
+      s.links.forEach((l) => {
+        this.sitesList.push(l)
+      })
+    })
+  }
 }
