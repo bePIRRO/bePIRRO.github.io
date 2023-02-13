fe15ffe76d080ccc4be2f9455cbad371060e3b99
bePIRRO <manuel.pirro@gmail.com>
2023-02-13 23:14:34 +0100

    feat: create card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,19 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { HeaderComponent } from './core/header/header.component';
-import { ListComponent } from './pages/list/list.component';
+import { HeaderComponent } from './core/header/header.component'
+import { ListComponent } from './pages/list/list.component'
 import { DetailComponent } from './pages/detail/detail.component'
+import { CardComponent } from './pages/list/card/card.component'
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, ListComponent, DetailComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ListComponent,
+    DetailComponent,
+    CardComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
